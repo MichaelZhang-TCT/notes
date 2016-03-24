f702105a8af25a810a6b0fef1b2ae203edbd7bf2
EthanYe <ethanye@foxmail.com>
2016-03-24 18:40:42 +0800

    Unity

---
+++ Unity/projects/HelloUnity5/Assets/Scripts/TransformTest.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformTest : MonoBehaviour {
+
+    public MoveDirection moveDirection = MoveDirection.Idle;
+
+    public enum MoveDirection { Idle, Forward, Right, Up }
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Vector3 directionVector;
+        switch (moveDirection)
+        {
+            case MoveDirection.Idle:
+                directionVector = Vector3.zero;
+                break;
+
+            case MoveDirection.Forward:
+                directionVector = transform.forward;
+                break;
+
+            case MoveDirection.Right:
+                directionVector = transform.right;
+                break;
+
+            case MoveDirection.Up:
+                directionVector = transform.up;
+                break;
+
+            default:
+                return;
+        }
+
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = directionVector * 1;
+	}
+}
---
+++ Unity/projects/HelloUnity5/Assets/Scripts/TransformTestDirection.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformTestDirection : MonoBehaviour {
+
+    public GameObject cube;
+
+    void Awake()
+    {
+        cube = GameObject.Find("TransformBullet");
+    }
+
+	// Use this for initialization
+	void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Rigidbody cubeRigidbody = cube.GetComponent<Rigidbody>();
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // pull
+            // 1. target point at local position
+            Vector3 sightPoint = new Vector3(0, 0, 3);
+            // 2. convert the target point to world position
+            Vector3 sightPointWorldPosition = transform.TransformPoint(sightPoint);
+            // 3. set the bullet's world position to target world position
+            cube.transform.position = sightPointWorldPosition;
+
+            // 4. make the bullet attach to sight point
+            cube.transform.parent = transform;
+            // 5. make it kinematic
+            cubeRigidbody.isKinematic = true;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            // push
+            if (cube.transform.parent == transform)
+            {
+                // 1. 
+                cubeRigidbody.isKinematic = false;
+                cube.transform.parent = null;
+
+                Vector3 worldDirection = transform.TransformDirection(0, 0, 1); // forward
+                cubeRigidbody.AddForce(worldDirection * 10, ForceMode.Impulse);
+            }
+        }
+	}
+}
